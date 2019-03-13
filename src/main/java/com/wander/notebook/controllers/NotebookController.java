d7a5db1ed56ea4629135ab756af99fe8321e7192
sachin <sachinjohar2@gmail.com>
2019-03-13 23:30:17 +0530

    Added cors

--- src/main/java/com/wander/notebook/controllers/NotebookController.java
+++ src/main/java/com/wander/notebook/controllers/NotebookController.java
@@ -8,6 +8,7 @@ import com.wander.notebook.services.NotebookService;
 import com.wander.notebook.services.UserService;
 import io.jsonwebtoken.lang.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("notebook")
+@CrossOrigin(origins = "http://localhost:1900")
 public class NotebookController {
 
     @Autowired
--- src/main/java/com/wander/notebook/controllers/UserController.java
+++ src/main/java/com/wander/notebook/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.wander.notebook.repositories.UserRepository;
 import com.wander.notebook.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:1900")
+
 public class UserController {
 
     @Autowired
