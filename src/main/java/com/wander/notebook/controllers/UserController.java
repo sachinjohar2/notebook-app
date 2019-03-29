05d35a4785530354efd574c2558ad9926b39b785
Prisha <PriyaRamesh.Shah@lntinfotech.com>
2019-03-29 14:58:16 +0530

    Update UserController.java
    Removed validator dependencies

--- src/main/java/com/wander/notebook/controllers/UserController.java
+++ src/main/java/com/wander/notebook/controllers/UserController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import com.wander.notebook.model.User;
 import com.wander.notebook.services.UserService;
-import com.wander.notebook.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
